88f3d8a8199816d6f4cb2d8e32ee587fc52338a3
natanngl03 <roy.natanaelngl03@gmail.com>
2026-08-07 18:57:29 +0700

    update konten & lighthouse performa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
-
 import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [icon(), react()],
+   site: "https://jayapasir.com/",
+   integrations: [icon(), react(), sitemap()],
    build: {
       assets: 'assets',
    },
--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,22 @@ type HandlerPage = 'react' | 'astro';
 interface GlobalProps {
    handler: HandlerPage;
 }
+
+interface ProductDetail {
+   title: string;
+   page: string;
+}
+
+
+interface ProductType {
+   params: {
+      product: string
+   },
+   title: string,
+   price: {
+      truck: string,
+      engkel: string,
+      pickup: string
+   },
+   image: string
+}
