6c428d19532554423ab0a16a577c65c6c982d053
natanngl03 <roy.natanaelngl03@gmail.com>
2026-08-27 20:54:45 +0700

    update analytic

---
+++ public/js/analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+   dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'AW-18350103200');
+gtag('event', 'conversion', { send_to: 'AW-18350103200/P_01CL_I--YcEKC1ga5E' });
